116b0599e0b694c30ca99c4538476e604f5f84ff
Ei8ht <chanon_champ@hotmail.com>
2018-10-25 17:07:25 +0700

    add log4j2

--- src/main/java/com/chanon/dev/SpringdevApplication.java
+++ src/main/java/com/chanon/dev/SpringdevApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringdevApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdevApplication.class, args);
 	}
--- src/main/java/com/chanon/dev/controller/InitialController.java
+++ src/main/java/com/chanon/dev/controller/InitialController.java
@@ -1,14 +1,21 @@
 package com.chanon.dev.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class InitialController {
-	
+	private static final Logger logger = LogManager.getLogger();
 	@RequestMapping("/index")
 	public String initForm() {
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
 		return "index";
 	}
 }
