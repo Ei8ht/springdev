54c036b298c3207298fe902a332d1884a9ead88d
Ei8ht <chanon_champ@hotmail.com>
2018-10-25 15:05:24 +0700

    add webjar to project

--- src/main/java/com/chanon/dev/config/WebSecurityConfig.java
+++ src/main/java/com/chanon/dev/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.chanon.dev.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -18,24 +20,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 	
-//	@Override
-//	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// -------------------- Embed ldap ------------------- //
-//		auth.ldapAuthentication()
-//			.userDnPatterns("uid={0},ou=people")
-//			.groupSearchBase("ou=groups")
-//			.contextSource()
-//				.url("ldap://localhost:8389/dc=springframework,dc=org")
-//				.and()
-//			.passwordCompare()
-//				.passwordEncoder(new LdapShaPasswordEncoder())
-//				.passwordAttribute("userPassword");
+		auth.ldapAuthentication()
+			.userDnPatterns("uid={0},ou=people")
+			.groupSearchBase("ou=groups")
+			.contextSource()
+				.url("ldap://localhost:8389/dc=springframework,dc=org")
+				.and()
+			.passwordCompare()
+				.passwordEncoder(new LdapShaPasswordEncoder())
+				.passwordAttribute("userPassword");
 		
 		// -------------------- KTB ldap ------------------- //
 //		auth.ldapAuthentication()
 //			.userDnPatterns("uid={0},ou=people,o=ktb.co.th")
 //			.groupSearchBase("ou=people")
 //			.contextSource().url("ldap://10.2.63.76:602/o=kcs");
-//	}
+	}
 	
 }
